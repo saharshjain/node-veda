44b9df0ab96e57791639506999a64690f8dce4af
saharshjain <saharshjain94@outlook.com>
2020-05-26 23:05:14 +0530

    calculating hamming distance

--- src/util/math-functions.js
+++ src/util/math-functions.js
@@ -6,6 +6,21 @@ function calculateGCD(number1, number2) {
   }
 }
 
+//chnage to enable it for all data types
+function hammingDistanceArray(arrayOne, arrayTwo) {
+  if (arrayOne.length != arrayTwo.length) {
+    return -1;
+  }
+  let hammingDistance = 0;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] != arrayTwo[i]) {
+      hammingDistance++;
+    }
+  }
+  return hammingDistance;
+}
+
 module.exports = {
-  calculateGCD
+  calculateGCD,
+  hammingDistanceArray
 }
