bd9fc282947a47e5fc7b44dd51b067e7c4d7b68d
saharshjain <saharshjain94@outlook.com>
2020-09-01 22:29:06 +0530

    Move all zeres to end

---
+++ src/array/array-rearrangement/zero-to-end.js
@@ -0,0 +1,28 @@
+//Have to rearrange array in such a form that all occuring zeroes are at the end
+//while maintaining the array original order
+//e.g. array = [1,0,0,3,0,2,0,1]
+//output should be  = [1,3,2,1,0,0,0,0]
+
+function zeroToEnd(array) {
+  let slow = 0;
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    counter++;
+    if (array[i] == 0) {
+      if (slow < i) {
+        slow = i;
+      }
+      while (array[slow] == 0 && slow < array.length) {
+        counter++;
+        slow++;
+      }
+      if (slow == array.length) {
+        break;
+      }
+      array[i] = array[slow]
+      array[slow] = 0;
+    }
+  }
+  return array;
+}
+console.log(zeroToEnd([1, 0, 0, 3, 0, 2, 0, 1]));
