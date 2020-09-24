2e77eb709b9d1771c6ba2044958a5e24a3611362
saharshjain <saharshjain94@outlook.com>
2020-09-24 23:13:02 +0530

    Fibonacci series using memoization

---
+++ src/array/array-processing/fibonacci_memoization.js
@@ -0,0 +1,19 @@
+// Implement fibonacci series using memoization which displays the nth number in fibonacci series starting with 0
+// input n = 10
+// output = 55
+
+
+let dict = {};
+
+function fibonacciMemoization(n) {
+  if (dict[n] == undefined) {
+    if (n <= 1) {
+      dict[n] = n;
+    } else {
+      dict[n] = fibonacciMemoization(n - 1) + fibonacciMemoization(n - 2);
+    }
+  }
+  return dict[n];
+}
+
+console.log(fibonacciMemoization(10));
