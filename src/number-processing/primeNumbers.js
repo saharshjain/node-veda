9f1d1179defe34bd2d2b0aed1b8cf4ab717899a6
saharshjain <saharshjain94@outlook.com>
2021-04-22 22:50:21 +0530

    adding more optimised solution for finding prime numbers

--- src/number-processing/primeNumbers.js
+++ src/number-processing/primeNumbers.js
@@ -1,10 +1,17 @@
 //find whether a number is prime or // NOTE:
 
 function isPrime(num) {
-  if (num === 1) {
+  if (num == 1) {
     return false;
   }
-  for (let i = 2; i < +(num / 2); i++) {
+  if (num == 2 || num == 3) {
+    return true;
+  }
+  if (num % 2 == 0 || num % 3 == 0) {
+    return false;
+  }
+
+  for (let i = 5; i < +(num / 2); i++) {
     if (num % i == 0) {
       return false;
     }
@@ -14,12 +21,18 @@ function isPrime(num) {
 
 console.log(isPrime(23));
 
-
 function isPrimeOptimised(num) {
-  if (num === 1) {
+  if (num == 1) {
     return false;
   }
-  for (let i = 2; i < +Math.sqrt(num); i++) {
+  if (num == 2 || num == 3) {
+    return true;
+  }
+  if (num % 2 == 0 || num % 3 == 0) {
+    return false;
+  }
+
+  for (let i = 5; i < +Math.sqrt(num); i++) {
     if (num % i == 0) {
       return false;
     }
@@ -27,3 +40,24 @@ function isPrimeOptimised(num) {
   return true;
 }
 console.log(isPrimeOptimised(23));
+
+
+function isPrimeUltraOptimised(num) {
+  if (num == 1) {
+    return false;
+  }
+  if (num == 2 || num == 3) {
+    return true;
+  }
+  if (num % 2 == 0 || num % 3 == 0) {
+    return false;
+  }
+
+  for (let i = 5; i * i < num; i += 6) {
+    if (num % i == 0 || num % (i + 2) == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isPrimeUltraOptimised(23));
