c897d1b4afc6b35a0702c569feaff17f173609fd
saharshjain <saharshjain94@outlook.com>
2021-05-12 23:18:41 +0530

    word combination from phone digits

---
+++ src/string-processing/wordsFromPhoneDigits.js
@@ -0,0 +1,23 @@
+//Possible Words From Phone Digits
+const CHARACTER_MAP = {
+  2: "ABC",
+  3: "DEF",
+  4: "GHI",
+  5: "JKL",
+  6: "MNO",
+  7: "PQRS",
+  8: "TUV",
+  9: "WXYZ"
+}
+
+function wordsFromPhoneDigits(arr, auxString, index) {
+  if (index == arr.length) {
+    console.log(auxString);
+    return;
+  }
+  for (let i = 0; i < CHARACTER_MAP[arr[index]].length; i++) {
+    wordsFromPhoneDigits(arr, auxString + CHARACTER_MAP[arr[index]][i], index + 1);
+  }
+}
+
+wordsFromPhoneDigits([7, 2, 2], '', 0);
