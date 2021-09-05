05f43764aa569abdb2bf307539bbe34c46549b97
saharshjain <saharshjain94@outlook.com>
2021-09-05 22:46:52 +0530

    no. of edits in a string to equalise to another string

---
+++ src/dynamicProgramming/editDistanceRecursion.js
@@ -0,0 +1,23 @@
+// check difference between 2 string and operation to make them equal
+// use insert, replace and delete ops only
+
+function editDistance(str1, str2, m, n) {
+  console.log(m, n);
+  if (m == 0) {
+    return n;
+  }
+  if (n == 0) {
+    return m;
+  }
+  if (str1[m - 1] == str2[n - 1]) {
+    return editDistance(str1, str2, m - 1, n - 1);
+  } else {
+    return 1 + Math.min(
+      editDistance(str1, str2, m, n - 1),
+      editDistance(str1, str2, m - 1, n),
+      editDistance(str1, str2, m - 1, n - 1)
+    );
+  }
+}
+
+console.log(editDistance("saharsh", "isha", 7, 4));
